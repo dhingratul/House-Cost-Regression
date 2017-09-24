8f812facfb7c572d27560a561ced29d52e753c0c
dhingratul <dhingra.atul92@gmail.com>
2017-09-24 14:42:09 -0400

    Addedd LeakyRelu

--- src/NeuralRegression.py
+++ src/NeuralRegression.py
@@ -9,18 +9,21 @@ Created on Thu Sep 21 12:19:40 2017
 import numpy as np
 import pandas as pd
 from keras.models import Sequential
-from keras.layers import Dense, Activation
+from keras.layers import Dense
 from keras.wrappers.scikit_learn import KerasRegressor
 from sklearn.preprocessing import StandardScaler
 from sklearn.cross_validation import train_test_split
+from sklearn.metrics import mean_squared_error
+from keras.layers.advanced_activations import LeakyReLU
 
 
 def nonLinearModel():
     model = Sequential()
-    model.add(Dense(20, input_dim=13, kernel_initializer='normal'))
-    model.add(Activation('relu'))
-    model.add(Dense(1, kernel_initializer='normal'))
-    model.add(Activation('relu'))
+    model.add(Dense(20, input_dim=13, kernel_initializer='normal',
+                    activation='linear'))
+    model.add(LeakyReLU(alpha=0.1))
+    model.add(Dense(1, kernel_initializer='normal', activation='linear'))
+    model.add(LeakyReLU(alpha=0.1))
     model.compile(loss='mean_squared_error', optimizer='adam')
     return model
 
@@ -32,6 +35,7 @@ df = pd.read_csv(file, delim_whitespace=True, header=None)
 # split into X and y
 X = df.iloc[:, 0:13].as_matrix()
 y = df.iloc[:, 13].as_matrix()
+num = X.shape[1]
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1,
                                                     random_state=0)
 
@@ -44,5 +48,5 @@ clf = KerasRegressor(build_fn=nonLinearModel, nb_epoch=10, batch_size=5,
                      verbose=1)
 clf.fit(X_train, y_train)
 res = clf.predict(X_test)
-score = clf.score(X_test, y_test)
-print(score)
+mse = mean_squared_error(y_test, res)
+print("\n MSE DL:", mse)
